b0f8e3e56ac834dc3fb16073752cecb3e7735ddb
RainingChain <rainingchain@gmail.com>
2014-03-28 09:32:21 -0400

    contribution

--- server/client/js/shared/TODO.js
+++ server/client/js/shared/TODO.js
@@ -124,4 +124,44 @@ waypoint window
 
 
 
+Contribution Rewards:
+
+Custom Character Design:
+You can choose how your character look. Submit your very own sprite or use one already in the game engine.
+Ex: Any NPC/Monster/Boss
+
+Player Symbol:
+Add a symbol in front of your name in the chat box.
+Bronze Star:
+Silver Star:
+Gold Star:
+Custom Icon:
+
+Change Display Name:
+Change the name showed in-game. The username used to sign in will remain the same.
+
+Display announcement on website:
+Your message will appear for 24 hours starting at midnight East-Coast the day after your request.
+No offensive/IRL ads allowed.
+Cost/Character.
+
+Change bullet image:
+Change bullet image for one of your ability. Must be a bullet attack. The image/sprite can be anything. Limit 20 Ko.
+
+Broadcast Achievement:
+Possible broadcast: Level-up, Quest complete, Item Found,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
